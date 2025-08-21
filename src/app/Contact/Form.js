ae65add9e541962511fb548f74c1ed0ce679f368
sahil952988 <sahilacc000@gmail.com>
2025-08-21 22:34:05 +0545

    big-screen-resposive

--- src/app/Contact/Form.js
+++ src/app/Contact/Form.js
@@ -5,7 +5,19 @@ const Form = () => {
     <>
       <div className="max-w-[1536px] mx-auto">
 
+        <div className="map absolute right-[330px] mt-[78px] ">
+          <iframe
+            className="w-[397px] h-[510px]"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28250.63159397516!2d85.34315915164419!3d27.737998208780265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1be5bea00987%3A0x70cf02a942554e97!2sKapan%2C%2044600!5e0!3m2!1sen!2snp!4v1755792334150!5m2!1sen!2snp"
+            width="600"
+            height="450"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
 
+
+        </div>
         <div className="parent bg-white rounded-[40px] w-[1202px] h-[738px] flex justify-between mx-[119px] pl-[111px]">
 
 
@@ -49,21 +61,10 @@ const Form = () => {
             </div>
           </div>
 
-
-          <div className="map absolute right-[330px] mt-[78px] ">
-            <iframe
-              className="w-[397px] h-[510px]"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28250.63159397516!2d85.34315915164419!3d27.737998208780265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1be5bea00987%3A0x70cf02a942554e97!2sKapan%2C%2044600!5e0!3m2!1sen!2snp!4v1755792334150!5m2!1sen!2snp"
-              width="600"
-              height="450"
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            />
-
-
-          </div>
         </div>
+
+
+
       </div>
     </>
   )
